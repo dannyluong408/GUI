650ce488776a2abb16bf099aaba3403fcfbf396c
dannyluong408 <dannyluong408@gmail.com>
2016-11-21 13:57:23 -0800

    revised guild frame

--- guildframe2.cpp
+++ guildframe2.cpp
@@ -7,53 +7,51 @@ GuildFrame2::GuildFrame2(QWidget *parent)
     setFrameShape(QFrame::StyledPanel);
     setFrameShadow(QFrame::Raised);
     setGeometry(200,200,300,400);
+    setMinimumWidth(200);
 
-    verticalLayout = new QVBoxLayout(this);
-    verticalLayout->setSpacing(0);
-    verticalLayout->setContentsMargins(0,0,0,0);
-    verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
-    verticalLayout->setContentsMargins(0, 0, 0, 0);
-    frame_3 = new QFrame(this);
-    frame_3->setObjectName(QStringLiteral("frame_3"));
-    frame_3->setFrameShape(QFrame::StyledPanel);
-    frame_3->setFrameShadow(QFrame::Raised);
-    horizontalLayout_3 = new QHBoxLayout(frame_3);
+    mainLayout = new QVBoxLayout(this);
+    mainLayout->setSpacing(0);
+    mainLayout->setContentsMargins(0,0,0,0);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+    Frame1 = new QFrame(this);
+    Frame1->setObjectName(QStringLiteral("Frame1"));
+    Frame1->setFrameShape(QFrame::StyledPanel);
+    Frame1->setFrameShadow(QFrame::Raised);
+    horizontalLayout_3 = new QHBoxLayout(Frame1);
     horizontalLayout_3->setSpacing(0);
     horizontalLayout_3->setContentsMargins(0,0,0,0);
     horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
-    Icon = new QLabel(frame_3);
+    Icon = new QLabel(Frame1);
     Icon->setObjectName(QStringLiteral("Icon"));
-    QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-    sizePolicy.setHorizontalStretch(0);
-    sizePolicy.setVerticalStretch(0);
-    sizePolicy.setHeightForWidth(Icon->sizePolicy().hasHeightForWidth());
+    QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
     Icon->setSizePolicy(sizePolicy);
-    Icon->setMaximumSize(QSize(100, 100));
-    Icon->setBaseSize(QSize(25, 25));
-    Icon->setPixmap(QPixmap(":/ui/images/panda.jpg").scaled(25,25,Qt::KeepAspectRatio));
-    Icon->setScaledContents(true);
+    Icon->setFixedSize(40,40);
+    Icon->setPixmap(QPixmap(":/ui/images/panda.jpg").scaled(40,40,Qt::KeepAspectRatio));
 
     horizontalLayout_3->addWidget(Icon);
+    horizontalLayout_3->setStretch(0,2);
+    horizontalLayout_3->setStretch(1,1);
 
-    frame_5 = new QFrame(frame_3);
-    frame_5->setObjectName(QStringLiteral("frame_5"));
-    frame_5->setFrameShape(QFrame::StyledPanel);
-    frame_5->setFrameShadow(QFrame::Raised);
-    verticalLayout_2 = new QVBoxLayout(frame_5);
+    subFrame1 = new QFrame(Frame1);
+    subFrame1->setObjectName(QStringLiteral("subFrame1"));
+    subFrame1->setFrameShape(QFrame::StyledPanel);
+    subFrame1->setFrameShadow(QFrame::Raised);
+    verticalLayout_2 = new QVBoxLayout(subFrame1);
     verticalLayout_2->setSpacing(0);
     verticalLayout_2->setContentsMargins(0,0,0,0);
     verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
     verticalLayout_2->setContentsMargins(0, 0, 0, 0);
-    frame_6 = new QFrame(frame_5);
-    frame_6->setObjectName(QStringLiteral("frame_6"));
-    frame_6->setFrameShape(QFrame::StyledPanel);
-    frame_6->setFrameShadow(QFrame::Raised);
-    horizontalLayout_4 = new QHBoxLayout(frame_6);
+    titleFrame = new QFrame(subFrame1);
+    titleFrame->setObjectName(QStringLiteral("titleFrame"));
+    titleFrame->setFrameShape(QFrame::StyledPanel);
+    titleFrame->setFrameShadow(QFrame::Raised);
+    horizontalLayout_4 = new QHBoxLayout(titleFrame);
     horizontalLayout_4->setSpacing(0);
     horizontalLayout_4->setContentsMargins(0,0,0,0);
     horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
     horizontalLayout_4->setContentsMargins(0, 0, 0, 0);
-    Title = new QLabel(frame_6);
+    Title = new QLabel(titleFrame);
     Title->setObjectName(QStringLiteral("Title"));
     QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Preferred);
     sizePolicy1.setHorizontalStretch(0);
@@ -65,64 +63,75 @@ GuildFrame2::GuildFrame2(QWidget *parent)
 
     horizontalLayout_4->addWidget(Title);
 
-    XOut = new QPushButton(frame_6);
+    XOut = new QPushButton(titleFrame);
     XOut->setObjectName(QStringLiteral("XOut"));
     sizePolicy.setHeightForWidth(XOut->sizePolicy().hasHeightForWidth());
     XOut->setSizePolicy(sizePolicy);
     XOut->setBaseSize(QSize(15, 15));
+    XOut->setMinimumSize(15,15);
 
     horizontalLayout_4->addWidget(XOut);
 
     horizontalLayout_4->setStretch(0, 9);
     horizontalLayout_4->setStretch(1, 1);
 
-    verticalLayout_2->addWidget(frame_6);
+    verticalLayout_2->addWidget(titleFrame);
 
-    frame_7 = new QFrame(frame_5);
-    frame_7->setObjectName(QStringLiteral("frame_7"));
-    frame_7->setBaseSize(QSize(75, 15));
-    frame_7->setFrameShape(QFrame::StyledPanel);
-    frame_7->setFrameShadow(QFrame::Raised);
-    horizontalLayout_5 = new QHBoxLayout(frame_7);
+    buttonFrame = new QFrame(subFrame1);
+    buttonFrame->setObjectName(QStringLiteral("buttonFrame"));
+    buttonFrame->setBaseSize(QSize(75, 15));
+    buttonFrame->setFrameShape(QFrame::StyledPanel);
+    buttonFrame->setFrameShadow(QFrame::Raised);
+    horizontalLayout_5 = new QHBoxLayout(buttonFrame);
     horizontalLayout_5->setSpacing(0);
     horizontalLayout_5->setContentsMargins(0,0,0,0);
     horizontalLayout_5->setObjectName(QStringLiteral("horizontalLayout_5"));
     horizontalLayout_5->setContentsMargins(0, 0, 0, 0);
-    pushButton_4 = new QPushButton(frame_7);
-    pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
-    sizePolicy.setHeightForWidth(pushButton_4->sizePolicy().hasHeightForWidth());
-    pushButton_4->setSizePolicy(sizePolicy);
+    information = new QPushButton(buttonFrame);
+    information->setObjectName(QStringLiteral("information"));
+    sizePolicy.setHeightForWidth(information->sizePolicy().hasHeightForWidth());
+    information->setSizePolicy(sizePolicy);
+    information->setFixedSize(QSize(65, 15));
+    information->setMinimumWidth(65);
+    information->setText("Information");
+    horizontalLayout_5->addWidget(information);
 
-    horizontalLayout_5->addWidget(pushButton_4);
+    settings = new QPushButton(buttonFrame);
+    settings->setObjectName(QStringLiteral("settings"));
+    sizePolicy.setHeightForWidth(settings->sizePolicy().hasHeightForWidth());
+    settings->setSizePolicy(sizePolicy);
+    settings->setFixedSize(QSize(45, 15));
+    settings->setMinimumWidth(45);
+    settings->setText("Settings");
+    horizontalLayout_5->addWidget(settings);
 
-    pushButton_2 = new QPushButton(frame_7);
-    pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
-    sizePolicy.setHeightForWidth(pushButton_2->sizePolicy().hasHeightForWidth());
-    pushButton_2->setSizePolicy(sizePolicy);
-    pushButton_2->setBaseSize(QSize(75, 15));
+    addmember = new QPushButton(buttonFrame);
+    addmember->setObjectName(QStringLiteral("addmember"));
+    sizePolicy.setHeightForWidth(addmember->sizePolicy().hasHeightForWidth());
+    addmember->setSizePolicy(sizePolicy);
+    addmember->setFixedSize(QSize(65, 15));
+    addmember->setMinimumWidth(65);
+    addmember->setText("Add Member");
+    horizontalLayout_5->addWidget(addmember);
 
-    horizontalLayout_5->addWidget(pushButton_2);
+    hSpacer = new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    horizontalLayout_5->addItem(hSpacer);
+    horizontalLayout_5->setStretch(0,2);
+    horizontalLayout_5->setStretch(1,1);
+    horizontalLayout_5->setStretch(2,2);
+    horizontalLayout_5->setStretch(3,1);
 
-    pushButton_3 = new QPushButton(frame_7);
-    pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
-    sizePolicy.setHeightForWidth(pushButton_3->sizePolicy().hasHeightForWidth());
-    pushButton_3->setSizePolicy(sizePolicy);
-    pushButton_3->setBaseSize(QSize(75, 15));
+    verticalLayout_2->addWidget(buttonFrame);
 
-    horizontalLayout_5->addWidget(pushButton_3);
-
-
-    verticalLayout_2->addWidget(frame_7);
-
-    verticalSpacer = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Expanding);
+    verticalSpacer = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Maximum);
 
     verticalLayout_2->addItem(verticalSpacer);
 
 
-    horizontalLayout_3->addWidget(frame_5);
+    horizontalLayout_3->addWidget(subFrame1);
 
 
-    verticalLayout->addWidget(frame_3);
+    mainLayout->addWidget(Frame1);
 
     Frame2 = new QFrame(this);
     Frame2->setObjectName(QStringLiteral("Frame2"));
@@ -135,15 +144,14 @@ GuildFrame2::GuildFrame2(QWidget *parent)
     horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
     tableView = new QTableView(Frame2);
     tableView->setObjectName(QStringLiteral("tableView"));
-    sizePolicy.setHeightForWidth(tableView->sizePolicy().hasHeightForWidth());
-    tableView->setSizePolicy(sizePolicy);
+    tableView->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
     tableView->setMaximumSize(QSize(897979, 16777215));
     tableView->setBaseSize(QSize(280, 230));
 
     horizontalLayout_2->addWidget(tableView);
 
 
-    verticalLayout->addWidget(Frame2);
+    mainLayout->addWidget(Frame2);
 
     Frame3 = new QFrame(this);
     Frame3->setObjectName(QStringLiteral("Frame3"));
@@ -160,18 +168,17 @@ GuildFrame2::GuildFrame2(QWidget *parent)
 
     horizontalLayout->addWidget(label);
 
-    checkBox = new QCheckBox(Frame3);
-    checkBox->setObjectName(QStringLiteral("checkBox"));
+    showoffline = new QCheckBox(Frame3);
+    showoffline->setObjectName(QStringLiteral("showoffline"));
 
-    horizontalLayout->addWidget(checkBox);
+    horizontalLayout->addWidget(showoffline);
 
 
-    verticalLayout->addWidget(Frame3);
+    mainLayout->addWidget(Frame3);
 
     Frame4 = new QFrame(this);
     Frame4->setObjectName(QStringLiteral("Frame4"));
-    sizePolicy1.setHeightForWidth(Frame4->sizePolicy().hasHeightForWidth());
-    Frame4->setSizePolicy(sizePolicy1);
+    Frame4->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     Frame4->setFrameShape(QFrame::StyledPanel);
     Frame4->setFrameShadow(QFrame::Raised);
     verticalLayout_3 = new QVBoxLayout(Frame4);
@@ -180,8 +187,7 @@ GuildFrame2::GuildFrame2(QWidget *parent)
     verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
     textBrowser = new QTextBrowser(Frame4);
     textBrowser->setObjectName(QStringLiteral("textBrowser"));
-    sizePolicy1.setHeightForWidth(textBrowser->sizePolicy().hasHeightForWidth());
-    textBrowser->setSizePolicy(sizePolicy1);
+    textBrowser->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     textBrowser->setSizeIncrement(QSize(0, 0));
     textBrowser->setBaseSize(QSize(280, 90));
     textBrowser->setStyleSheet(QStringLiteral("border-color: rgb(255, 255, 255);"));
@@ -189,22 +195,57 @@ GuildFrame2::GuildFrame2(QWidget *parent)
     verticalLayout_3->addWidget(textBrowser);
 
 
-    verticalLayout->addWidget(Frame4);
+    mainLayout->addWidget(Frame4);
 
-    verticalLayout->setStretch(0, 1);
-    verticalLayout->setStretch(1, 9);
-    verticalLayout->setStretch(2, 1);
-    verticalLayout->setStretch(3, 1);
+    mainLayout->setStretch(0, 1);
+    mainLayout->setStretch(1, 9);
+    mainLayout->setStretch(2, 1);
+    mainLayout->setStretch(3, 1);
 
     Icon->setText(QString());
-    Title->setText(QApplication::translate("MainWindow", "Both My Dads Play Druids", 0));
-    XOut->setText(QApplication::translate("MainWindow", "X", 0));
-    pushButton_4->setText(QApplication::translate("MainWindow", "PushButton", 0));
-    pushButton_2->setText(QApplication::translate("MainWindow", "PushButton", 0));
-    pushButton_3->setText(QApplication::translate("MainWindow", "PushButton", 0));
-    label->setText(QApplication::translate("MainWindow", "Online Now: 1/3", 0));
-    checkBox->setText(QApplication::translate("MainWindow", "CheckBox", 0));
-    textBrowser->setPlaceholderText(QString());
+    Title->setText("Both My Dads Play Druids");
+    XOut->setText("X");
+    label->setText("Online Now: 1/3");
+    showoffline->setText("showoffline");
+    textBrowser->setPlaceholderText(QString("GMOTD: Welcome to Both My Dads Play Druids!"));
 
     hide();
+}
+
+void GuildFrame2::resizeMe(QSize newSize){
+    double scale_x = 300.0/1200.0;
+    double scale_y = 400.0/900.0;
+    double scale;
+    double new_w = newSize.width();
+    double new_h = newSize.height();
+
+    //parent frame
+    resize(new_w*scale_x,
+           new_h*scale_y);
+
+    if (new_w < new_h){
+        scale = 40.0/1200.0;
+    }
+    else{
+        scale = 40.0/900.0;
+    }
+    Icon->setFixedSize(new_h*scale,
+                         new_h*scale);
+    Icon->resize(new_h*scale,
+                 new_h*scale);
+
+    //Resize Fonts
+    QFont font = Title->font();
+
+    if (new_w < 800.0){
+            font.setPointSize(7);
+    }
+    else{
+            font.setPointSize(8);
+    }
+    Title->setFont(font);
+    information->setFont(font);
+    settings->setFont(font);
+    addmember->setFont(font);
+
 }
--- guildframe2.hpp
+++ guildframe2.hpp
@@ -12,34 +12,36 @@ class GuildFrame2 : public QFrame
     Q_OBJECT
     public:
         GuildFrame2(QWidget *parent);
+    public slots:
+        void resizeMe(QSize newSize);
+
     private:
-        QVBoxLayout *verticalLayout;
-        QFrame *frame_3;
+        QVBoxLayout *mainLayout;
+        QFrame *Frame1;
         QHBoxLayout *horizontalLayout_3;
         QLabel *Icon;
-        QFrame *frame_5;
+        QFrame *subFrame1;
         QVBoxLayout *verticalLayout_2;
-        QFrame *frame_6;
+        QFrame *titleFrame;
         QHBoxLayout *horizontalLayout_4;
         QLabel *Title;
         QPushButton *XOut;
-        QFrame *frame_7;
+        QFrame *buttonFrame;
         QHBoxLayout *horizontalLayout_5;
-        QPushButton *pushButton_4;
-        QPushButton *pushButton_2;
-        QPushButton *pushButton_3;
-        QSpacerItem *verticalSpacer;
+        QPushButton *information;
+        QPushButton *settings;
+        QPushButton *addmember;
+        QSpacerItem *verticalSpacer,*hSpacer;
         QFrame *Frame2;
         QHBoxLayout *horizontalLayout_2;
         QTableView *tableView;
         QFrame *Frame3;
         QHBoxLayout *horizontalLayout;
         QLabel *label;
-        QCheckBox *checkBox;
+        QCheckBox *showoffline;
         QFrame *Frame4;
         QVBoxLayout *verticalLayout_3;
         QTextBrowser *textBrowser;
-
 };
 
 #endif // GUILDFRAME2_HPP
